953ab51b9da65546b422eb16cbad58d9094359cd
huynhthienkim21 <huynhthienkim21@gmail.com>
2026-04-27 00:53:09 +0700

    Update cache.js

--- core/cache.js
+++ core/cache.js
@@ -1,19 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-// ===== CACHE RAM =====
 let cache = {
   tietkhi: null,
   solarLunar: null
 };
 
-// ===== LOAD 1 LẦN =====
 function loadCache(){
 
   if(!cache.tietkhi){
     cache.tietkhi = JSON.parse(
       fs.readFileSync(
-        path.join(__dirname, './dataset/tietkhi-2026.json'),
+        path.join(process.cwd(), 'data/tietkhi-2026.json'),
         'utf-8'
       )
     );
@@ -22,14 +20,13 @@ function loadCache(){
   if(!cache.solarLunar){
     cache.solarLunar = JSON.parse(
       fs.readFileSync(
-        path.join(__dirname, './dataset/solar-lunar-2026.json'),
+        path.join(process.cwd(), 'data/solar-lunar-2026.json'),
         'utf-8'
       )
     );
   }
 }
 
-// ===== GET =====
 function getTietKhi(){
   loadCache();
   return cache.tietkhi;
