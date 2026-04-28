94b4d9bf41bd67bee2247c4e04a166e15e9e8d13
huynhthienkim21 <huynhthienkim21@gmail.com>
2026-04-28 13:15:21 +0700

    Update lunar-core.js

--- core/lunar-core.js
+++ core/lunar-core.js
@@ -1,9 +1,12 @@
 const { getSolarLunar } = require('./dataset');
 
+const CAN = ["Giáp","Ất","Bính","Đinh","Mậu","Kỷ","Canh","Tân","Nhâm","Quý"];
+const CHI = ["Tý","Sửu","Dần","Mão","Thìn","Tỵ","Ngọ","Mùi","Thân","Dậu","Tuất","Hợi"];
+
+// ===== LUNAR =====
 function convertSolar2Lunar(dd, mm, yy){
   const data = getSolarLunar(yy);
   const key = `${dd}/${mm}/${yy}`;
-
   return data[key] || null;
 }
 
@@ -20,7 +23,35 @@ function convertLunar2Solar(dd, mm, yy){
   return null;
 }
 
+// ===== CAN CHI =====
+function getYearCanChi(year){
+  return CAN[(year+6)%10] + " " + CHI[(year+8)%12];
+}
+
+function jdSimple(dd, mm, yy){
+  return Math.floor(Date.UTC(yy, mm-1, dd)/86400000 + 2440587.5);
+}
+
+function getDayCanChi(dd, mm, yy){
+  const jd = jdSimple(dd, mm, yy);
+  return CAN[(jd+9)%10] + " " + CHI[(jd+1)%12];
+}
+
+function getHourCanChi(dd, mm, yy, hour){
+  const jd = jdSimple(dd, mm, yy);
+  const dayCanIndex = (jd+9)%10;
+  const chiIndex = Math.floor((hour+1)/2)%12;
+
+  const startCan = [0,2,4,6,8,0,2,4,6,8];
+  const canIndex = (startCan[dayCanIndex] + chiIndex) % 10;
+
+  return CAN[canIndex] + " " + CHI[chiIndex];
+}
+
 module.exports = {
   convertSolar2Lunar,
-  convertLunar2Solar
+  convertLunar2Solar,
+  getYearCanChi,
+  getDayCanChi,
+  getHourCanChi
 };
